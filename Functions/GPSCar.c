31c3676a38b00ee2a33e59b7309356e39f367349
Huy-Luu <73603476+Huy-Luu@users.noreply.github.com>
2021-07-22 15:20:52 +0700

    Add files via upload

--- Functions/GPSCar.c
+++ Functions/GPSCar.c
@@ -31,7 +31,13 @@ float convert(int16_t *x_axis, int16_t *y_axis, int16_t *z_axis, uint8_t compass
 	*y_axis = ((uint16_t)compassData[3]<<8)|compassData[2];
 	*z_axis = ((uint16_t)compassData[5]<<8)|compassData[4];
 	
-	return heading = atan2f((float)*x_axis, (float)*y_axis) *180/3.14;
+	heading = atan2f((float)*x_axis, (float)*y_axis) *180/3.14;
+	if(heading<0.0)
+	{
+		heading+=360.0;
+	}
+	
+	return heading;
 }
 
 //car speed
@@ -39,5 +45,93 @@ float speedCalc(float count, uint8_t time)
 {
 	float speed = (count/334)*(1000/time)*3.14*0/0.07;
 	return speed;
+}
+
+void forward()
+{
+	speed();
+	HAL_GPIO_WritePin(GPIOA,GPIO_PIN_10,0);// trái
+	HAL_GPIO_WritePin(GPIOB,GPIO_PIN_15,0);//phai
+}
+
+void backward()
+{
+	speed();
+	HAL_GPIO_WritePin(GPIOA,GPIO_PIN_10,1);// trái
+	HAL_GPIO_WritePin(GPIOB,GPIO_PIN_15,1);//phai
+}
+
+ void left()
+ {
+		htim1.Instance->CCR1=8000;
+	htim1.Instance->CCR2=10000;
+	HAL_GPIO_WritePin(GPIOA,GPIO_PIN_10,1);// trái
+	HAL_GPIO_WritePin(GPIOB,GPIO_PIN_15,0);//phai
+ }
+ 
+ void right()
+ {
+		htim1.Instance->CCR1=8000;
+	htim1.Instance->CCR2=10000;
+	HAL_GPIO_WritePin(GPIOA,GPIO_PIN_10,0);// trái
+	HAL_GPIO_WritePin(GPIOB,GPIO_PIN_15,1);//phai
+ }
+ 
+ void stop()
+ {
+	htim1.Instance->CCR1=0;
+	htim1.Instance->CCR2=0;
+ }
+ 
+ void speed()
+{
+	htim1.Instance->CCR1=9000;
+	htim1.Instance->CCR2=9000;
+}
+
+void PID()
+{
+		stable_time=HAL_GetTick();
+		
+		
+		//lay so dem ban dau cho tinh toan toc do
+		pre_count	=	TIM2->CNT;
+		pre_count_right	=	TIM3->CNT;
+		pre_period=HAL_GetTick();
+		HAL_Delay(10);
+		
+		//lay so dem sau va tinh toan thoi gian
+		count	=	TIM2->CNT;
+		count_right	=	TIM3->CNT;
+		period=HAL_GetTick();
+		sample_time=period-pre_period;
+		
+		//tinh toan toc do cho ca 2 dong co
+		actual_speed_right=((float)(count_right-pre_count_right))/334*((1000/sample_time)*3.14*0.07);
+		//actual_speed=((float)(count-pre_count))/334*((1000/sample_time)*3.14*0.07);
+			//actual_speed_right = speedCalc((float)(pre_count_right-count_right), sample_time);
+			//actual_speed = speedCalc((float)(pre_count-count), sample_time);
+		
+		// tinh toan PID cho dong co neu nhu khong co sai so o encoder.
+		//if((count<60000) ) 
+		//{
+		period=HAL_GetTick();
+		PWM=calculate((float)(period-pre_period)/1000, 0.9, actual_speed_right,0);
+		PWM_right=PWM;		
+		//}
+}
+
+float PIDAngle(float goal,float heading)
+{
+		
+	
+		pre_period=HAL_GetTick();
+		HAL_Delay(10);
+		period=HAL_GetTick();
+		sample_time=period-pre_period;
+
+		
+		return calculate_angle(sample_time, goal, heading);
+		
 }
 
--- Functions/GPSCar.h
+++ Functions/GPSCar.h
@@ -1,7 +1,7 @@
 #ifndef _GPSCAR_H
 #define _GPSCAR_H
 
-//#include "main.h"
+#include "main.h"
 #include "stm32f1xx_hal.h"
 #include "stm32f1xx_hal_i2c.h"
 #include <math.h>
@@ -12,7 +12,23 @@
 #define compass 0x1A
 
 extern I2C_HandleTypeDef hi2c1;
+extern TIM_HandleTypeDef htim1;
 
+	extern uint16_t repeated_time,count,pre_count;
+	
+	//float pre_count, count;
+	
+	extern uint16_t pre_count_right, count_right;
+	
+	extern float  period, pre_period, sample_time;
+	
+	extern float stable_time;
+	
+	extern float actual_speed_right, PWM, PWM_right;
+	
+	
+	extern float calculate(float repeated_time, float desired_speed, float actual_speed, float coup);
+	extern float calculate_angle(float repeated_time, float desired_angle, float actual_angle);
 
 //functions
 void compassWrite(uint8_t data, uint8_t size);
@@ -20,8 +36,18 @@ void compassInit();
 void readAxis(uint8_t compassData[6]);
 float convert(int16_t *x_axis, int16_t *y_axis, int16_t *z_axis, uint8_t compassData[6]);
 
+//manual control
+void forward();
+void backward();
+void left();
+void right();
+void stop();
+void speed();
+
 //motor
 float speedCalc(float count, uint8_t time);
+void PID();
+float PIDAngle(float goal,float heading);
 
 #endif
 
